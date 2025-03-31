04b847ee698f9288326979195b64344288f13132
DiegoMayans <dmayansky@itba.edu.ar>
2025-03-31 00:24:11 -0300

    update/board and sync init🎁

--- src/master.c
+++ src/master.c
@@ -25,11 +25,13 @@ typedef struct {
     int seed;
 } argument_t;
 
-void create_player(char *executable, int fd[2], char *height, char *width);
+pid_t create_player(char *executable, int fd[2], char *height, char *width);
 void create_view(char *executable, char *height, char *width);
 void parse_arguments(argument_t *arguments, int argc, char *argv[]);
-int parse_childs(int argc, char *argv[], argument_t *arguments, int players_read_fds[]);
+int parse_childs(int argc, char *argv[], argument_t *arguments, int players_read_fds[], player_t players[]);
 void validate_arguments(int flag_players, int players, int views);
+void init_board(game_board_t *game_board, int width, int height, int players_count);
+void init_sync(game_sync_t *game_sync);
 
 int main(int argc, char *argv[]) {
     // Se crean las dos zonas de memoria compartida
@@ -37,7 +39,6 @@ int main(int argc, char *argv[]) {
                                         // players
 
     argument_t arguments = {"10", "10", 200, 10, 0};
-    int players;  // Opcion -p obligatoria
 
     parse_arguments(&arguments, argc, argv);
 
@@ -47,24 +48,94 @@ int main(int argc, char *argv[]) {
     game_sync_t *game_sync = (game_sync_t *)createSHM(GAME_SYNC_PATH, sizeof(game_sync_t));
     (void)game_sync;
 
-    players = parse_childs(argc, argv, &arguments, players_read_fds);
+    int players_count = parse_childs(argc, argv, &arguments, players_read_fds, game_board->players_list);
 
     // A partir de aca se escribe la logica del master
 
-    for (int i = 0; i < players; i++) {
+    // Primero inicializamos las memorias compartidas
+    init_board(game_board, atoi(arguments.width), atoi(arguments.height), players_count);
+    init_sync(game_sync);
+
+    for (int i = 0; i < players_count; i++) {
         close(players_read_fds[i]);  // Cerramos los pipes de escritura para los
                                      // players
     }
 
-    while (wait(NULL) > 0)
-        ;
+    while (wait(NULL) > 0);
 
     exit(EXIT_SUCCESS);
 }  // END MAIN
 
+/* GAME FUNCTIONS */
+
+void init_board(game_board_t *game_board, int width, int height, int players_count) {
+    // init_board no inicializa players_list porque se inicializa en el parseo de argumentos
+    game_board->width = width;
+    game_board->height = height;
+    game_board->player_count = players_count;
+    game_board->game_has_finished = false;
+
+    for (int i = 0; i < width * height; i++) {
+        game_board->board[i] = rand() % 9 + 1;
+    }
+
+    // Inicializar las posiciones x e y de los jugadores
+    for (int i = 0; i < players_count; i++) {
+        game_board->players_list[i].x = rand() % width;
+        game_board->players_list[i].y = rand() % height;
+    }
+
+    initialize_player_positions(game_board, width, height, players_count);
+}
+
+void init_sync(game_sync_t *game_sync) {
+    sem_init(&game_sync->print_needed, 1, 0);
+    sem_init(&game_sync->print_done, 1, 0);
+    sem_init(&game_sync->access_queue, 1, 1);
+    sem_init(&game_sync->game_state_access, 1, 1);
+    sem_init(&game_sync->count_access, 1, 1);
+    game_sync->players_reading_count = 0;
+}
+
+void initialize_player(player_t *player, pid_t pid) {
+    player->score = 0;
+    player->invalid_move_req_count = 0;
+    player->move_req_count = 0;
+    player->x = 0;
+    player->y = 0;
+    player->has_valid_moves = true;
+    player->pid = pid;
+}
+
+void initialize_player_positions(game_board_t *game_board, int width, int height, int players_count) {
+    // Estrategia: Se divide el tablero en celdas de igual tamaño. Se asigna una celda al azar a cada jugador.
+    // Las celdas son lo suficientemente grandes como para que si dos jugadores tienen celdas contiguas 
+    // sigan estando lo suficientemente espaciados.
+
+    int rows = (int) sqrt(players_count);
+    int cols = (players_count + rows - 1) / rows;  
+    int cell_width = width / cols;                 
+    int cell_height = height / rows;
+
+    int total_cells = rows * cols;
+    int cell_indices[total_cells]; // Arreglo con los índices de las celdas
+    for (int i = 0; i < total_cells; i++) {
+        cell_indices[i] = i;
+    }
+
+    // Se mezclan los índices de las celdas
+    for (int i = 0; i < total_cells; i++) {
+        int j = rand() % total_cells;
+        int temp = cell_indices[i];
+        cell_indices[i] = cell_indices[j];
+        cell_indices[j] = temp;
+    }// SEGUIR
+
+}
+
 /* AUXILIARY FUNCTIONS */
 
-void create_player(char *executable, int fd[2], char *height, char *width) {
+pid_t create_player(char *executable, int fd[2], char *height, char *width) {
     pid_t pid = fork();
     if (pid == -1) {
         perror("fork");
@@ -74,13 +145,13 @@ void create_player(char *executable, int fd[2], char *height, char *width) {
     if (pid == 0) {
         // Proceso hijo
         int i = fd[0];
-        while(i >= INITIAL_FD_COUNT){
-          close(i);
-          i--;
-        }                     // No necesita leer del pipe
-        close(STDOUT_FILENO); // Cerramos stdout
-        dup(fd[1]);           // Redirigimos stdout al pipe
-        close(fd[1]);         // Cerramos pipe después de duplicar
+        while (i >= INITIAL_FD_COUNT) {
+            close(i);
+            i--;
+        }  // No necesita leer del pipe
+        close(STDOUT_FILENO);  // Cerramos stdout
+        dup(fd[1]);            // Redirigimos stdout al pipe
+        close(fd[1]);          // Cerramos pipe después de duplicar
         close(STDOUT_FILENO);  // Cerramos stdout
         dup(fd[1]);            // Redirigimos stdout al pipe
         close(fd[1]);          // Cerramos pipe después de duplicar
@@ -92,6 +163,8 @@ void create_player(char *executable, int fd[2], char *height, char *width) {
         perror("execve");
         exit(EXIT_FAILURE);
     }
+
+    return pid;
 }
 
 void create_view(char *executable, char *height, char *width) {
@@ -145,8 +218,8 @@ void parse_arguments(argument_t *arguments, int argc, char *argv[]) {
     }
 }
 
-int parse_childs(int argc, char *argv[], argument_t *arguments, int players_read_fds[]) {
-    int players = 0, flag_players = 0, views = 0;
+int parse_childs(int argc, char *argv[], argument_t *arguments, int players_read_fds[], player_t players[]) {
+    int players_count = 0, flag_players = 0, views = 0;
     int i = 1;
     while (i < argc) {                     // Loop through args
         if (strcmp(argv[i], "-p") == 0) {  // -p args
@@ -154,15 +227,20 @@ int parse_childs(int argc, char *argv[], argument_t *arguments, int players_read
             i++;
 
             while (i < argc && argv[i][0] != '-') {  // Create player processes
-                if (players >= MAX_PLAYERS) {
+                if (players_count >= MAX_PLAYERS) {
                     fprintf(stderr, "Error: No se pueden tener mas de %d jugadores\n", MAX_PLAYERS);
                     exit(EXIT_FAILURE);
                 }
+
                 int fd[2];
                 pipe(fd);
-                create_player(argv[i], fd, arguments->height, arguments->width);
-                players_read_fds[players++] = fd[0];
+                pid_t pid = create_player(argv[i], fd, arguments->height, arguments->width);
+
+                initialize_player(&players[players_count], pid);
+
+                players_read_fds[players_count++] = fd[0];
                 close(fd[1]);
+
                 i++;
             }
         } else if (strcmp(argv[i], "-v") == 0) {  // -v args
@@ -182,9 +260,9 @@ int parse_childs(int argc, char *argv[], argument_t *arguments, int players_read
     }
 
     // Validate arguments
-    validate_arguments(flag_players, players, views);
+    validate_arguments(flag_players, players_count, views);
 
-    return players;
+    return players_count;
 }
 
 void validate_arguments(int flag_players, int players, int views) {
