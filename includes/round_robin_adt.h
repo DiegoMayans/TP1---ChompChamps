6b2e1781dd274d0232d12887744b9169dcd181db
emitchellitba <emitchell@itba.edu.ar>
2025-04-14 10:12:14 -0300

    fix: changed adt to list structure. Still has memory leaks. Fix

--- includes/round_robin_adt.h
+++ includes/round_robin_adt.h
@@ -11,20 +11,15 @@ typedef struct {
     int fd;
 } requester_t;
 
-//User must define an upper bound:
-#define MAX_REQUESTERS 9
-
-//Notas para nosotros:
 
 //Retorna un puntero a shared memory
-round_robin_adt new_round_robin(requester_t requesters[], int cant_requesters);
+round_robin_adt new_round_robin();
 //No retorna nada
-int push_request(round_robin_adt round_robin, requester_t requester);
+int push(round_robin_adt round_robin, requester_t* requester);
 //Retorna id de solicitudes restantes, -1 si no hay más solicitudes
-requester_t pop_request(round_robin_adt round_robin);
+requester_t* pop(round_robin_adt round_robin);
 //Retorna si dos requesters son iguales
-bool equals(requester_t req1, requester_t req2);
+bool equals(requester_t* req1, requester_t* req2);
 //Retorna si el id es valido
-bool is_id_valid (requester_t requester);
 
 #endif
--- src/round_robin_adt.c
+++ src/round_robin_adt.c
@@ -9,137 +9,223 @@ struct requester_key {
 	int id; //	>= 0
 };
 
-typedef struct {
-	requester_t requester;
+struct request_t{
+	requester_t* requester;
 	int requests_amount;
-} request_t;
-
-struct round_robin_cdt {
-	request_t requests_queue[MAX_REQUESTERS];
-	int requests_size;
-	requester_t priority_queue[MAX_REQUESTERS];
-	int priority_current_size;
-	int cant_requesters;
+	struct request_t * request_next;
+	struct request_t * priority_next;
 };
 
-static struct round_robin_cdt round_robin = {0};
+struct round_robin_cdt {
+	struct request_t * requests_first;
+	struct request_t * requests_last;
+	struct request_t * priority_first;
+	struct request_t * priority_last;
+	struct requester_key * valid_keys;
+	int cant_keys;
+};
 
-struct requester_key keys[MAX_REQUESTERS];
-static requester_id valid_id[MAX_REQUESTERS];
-
-round_robin_adt new_round_robin(requester_t requesters[], int cant_requesters) {
-	for(int i = 0; i < cant_requesters; i++) {
-		keys[i].id = i;
-		requesters[i].id = &keys[i];
-		valid_id[i] = &keys[i];
+round_robin_adt new_round_robin() {
+	round_robin_adt new = (round_robin_adt)malloc(sizeof(struct round_robin_cdt));
+	new->valid_keys = NULL;
+	new->cant_keys = 0;
+	if(new == NULL) {
+		perror("Memory allocation failed");
+        return NULL;
 	}
-	round_robin.requests_size = 0;
-	round_robin.cant_requesters = cant_requesters;
-	round_robin.priority_current_size = 0;
-	return &round_robin;
+	return new;
 }
 
-bool equals(requester_t req1, requester_t req2) {
-	return (req1.id->id - req2.id->id) == 0;
+static void assign_valid_key(round_robin_adt round_robin, requester_t* requester) {
+	struct requester_key* new_id = (struct requester_key*) malloc(sizeof(struct requester_key));
+	new_id->id = round_robin->cant_keys++;
+	requester->id = new_id;
 }
 
-static bool belongs(round_robin_adt round_robin, requester_t requester) {
-	bool found = false;
-	for(int i = 0; i < round_robin->priority_current_size && !found; i++) {
-		if(equals(requester, round_robin->priority_queue[i])) {
-			found = true;
-		}
-	}
-	return found;
+//In order to use a requester it must be first instantiated
+void instantiate_requester(round_robin_adt round_robin, requester_t* requester) {
+	assign_valid_key(round_robin, requester);
 }
 
-
-static void init_requester(round_robin_adt round_robin, requester_t requester, int index) {
-	round_robin->requests_queue[index].requester = requester;
-	round_robin->requests_queue[index].requests_amount = 1;
+void print_list(round_robin_adt rr) {
+	printf("Request List:\n");
+	struct request_t* current = rr->requests_first;
+	while(current != NULL) {
+		printf("\tfd: %d\n", current->requester->id->id);
+		current = current->request_next;
+	}
+	printf("Priority List:\n");
+	current = rr->priority_first;
+	while(current != NULL) {
+		printf("\tfd: %d\n", current->requester->id->id);
+		current = current->priority_next;
+	}
 }
 
-static int has_priority(requester_t incoming, requester_t active, round_robin_adt round_robin) {
-	int incoming_index, active_index;
-	for(int i = 0; i < round_robin->priority_current_size; i++) {
-		if(equals(incoming, round_robin->priority_queue[i])) {
-			incoming_index = i;
-		}
-		if(equals(active, round_robin->priority_queue[i])) {
-			active_index = i;
-		}
-	}
-	return incoming_index - active_index;
+bool equals(requester_t* req1, requester_t* req2) {
+	return (req1->id->id - req2->id->id) == 0;
 }
 
-int push_request(round_robin_adt round_robin, requester_t requester) {
-	if(!belongs(round_robin, requester)) {
-		bool found = false;
-		for(int i = 0; i < round_robin->cant_requesters && !found; i++) {
-			if(requester.id->id == valid_id[i]->id) {
-				round_robin->priority_queue[round_robin->priority_current_size++] = requester;
-				found = true;
-			}
-		}
-		if(!found) {
-			return EXIT_FAILURE;
-		}
+//If found, return the request_t node corresponding to the requester. If not return NULL
+static struct request_t* find_requester(round_robin_adt round_robin, requester_t* requester) {
+	struct request_t * current = round_robin->priority_first;
+	if(current == NULL) {
+		return NULL;
 	}
-	bool added = false;
-	int i = 0;
-	for(; i < round_robin->requests_size && !added; i++) {
-		int priority;
-		if((priority = has_priority(requester, round_robin->requests_queue[i].requester, round_robin)) < 0) {
-			for(int k = round_robin->requests_size; k > i; k--) {
-				round_robin->requests_queue[k] = round_robin->requests_queue[k - 1];
-			}
-			init_requester(round_robin, requester, i);
-			round_robin->requests_size++;
-			added = true;
-		} else if(priority == 0) {
-			round_robin->requests_queue[i].requests_amount++;
-			added = true;
+	while(current != NULL) {
+		if(equals(requester, current->requester)) {
+			return current;
 		}
+		current = current->priority_next;
 	}
-	if(!added) {
-		init_requester(round_robin, requester, i);
-		round_robin->requests_size++;
-	}
+	return NULL;
+}
 
+//Returns true if incoming has more priority than active
+static bool has_priority(requester_t* incoming, requester_t* active, round_robin_adt round_robin) {
+	struct request_t* current = round_robin->priority_first;
+	
+	while(current != NULL) {
+		if(equals(current->requester, incoming)) {
+			return true;
+		}
+		if(equals(current->requester, active)) {
+			return false;
+		}
+		current = current->priority_next;
+	}
+	return false;
+}
+
+//Associates requester to a request node
+static struct request_t* create_node(round_robin_adt round_robin, requester_t* requester) {
+	struct request_t* new = (struct request_t*) malloc(sizeof(struct request_t));
+	if(new == NULL) {
+		perror("Memory allocation failed");
+		return NULL;
+	}
+	new->requester = requester;
+	new->requests_amount = 1;
+	new->priority_next = NULL;
+	new->request_next = NULL;
+	if(round_robin->requests_first == NULL) {
+		round_robin->requests_first = new;
+		if(round_robin->priority_first == NULL) {
+			round_robin->priority_first = new;
+		}
+	}
+	if(round_robin->priority_last != NULL) {
+		round_robin->priority_last->priority_next = new;
+	}
+	round_robin->priority_last = new;
+	if(round_robin->requests_last != NULL) {
+		round_robin->requests_last->request_next = new;
+	}
+	round_robin->requests_last = new;
+}
+
+static struct request_t* push_helper(round_robin_adt round_robin, struct request_t* current, struct request_t* to_add) {
+	if(current == NULL) {
+		to_add->requests_amount = 1;
+		round_robin->requests_last->request_next = to_add;
+		return to_add;
+	}
+	if(equals(to_add->requester, current->requester)) {
+		current->requests_amount++;
+		return current;
+	} else if(has_priority(to_add->requester, current->requester, round_robin)) {
+		to_add->requests_amount = 1;
+		to_add->request_next = current;
+		return to_add;
+	} else {
+		current->request_next = push_helper(round_robin, current->request_next, to_add);
+		return current;
+	}
+}
+
+int push(round_robin_adt round_robin, requester_t* requester) {
+	struct request_t* requester_node = find_requester(round_robin, requester);
+	if(requester_node == NULL) {
+		create_node(round_robin, requester);
+	} else {
+		if(round_robin->requests_first == NULL) {
+			requester_node->requests_amount = 1;
+			round_robin->requests_first = requester_node;
+			round_robin->requests_last = requester_node;
+		} else {
+			round_robin->requests_first = push_helper(round_robin, round_robin->requests_first, requester_node);
+		}
+	}
 	return EXIT_SUCCESS;
 }
 
-requester_t pop_request(round_robin_adt round_robin) {
-	if(round_robin->requests_size <= 0) {
-		static struct requester_key error_key = {.id = -1}; 
-		requester_t to_return_error = {.id = &error_key};
 
-		return to_return_error;
+requester_t* pop(round_robin_adt round_robin) {
+	if(round_robin->requests_first == NULL) {
+		return NULL;
 	}
-	request_t to_return = round_robin->requests_queue[0];
 
-	for(int i = 1; i < round_robin->requests_size; i++) {
-		round_robin->requests_queue[i - 1] = round_robin->requests_queue[i];
+	//Request list management
+	struct request_t* to_return = round_robin->requests_first;
+	round_robin->requests_first = to_return->request_next;
+	if(--to_return->requests_amount > 0) {
+		//If there only was 1 element, and it had more than 1 request, we need for it to keep at request_first
+		if(to_return->request_next == NULL)
+			round_robin->requests_first = to_return;
+		round_robin->requests_last->request_next = to_return;
+		round_robin->requests_last = to_return;
 	}
-	if((--to_return.requests_amount) > 0) {
-		round_robin->requests_queue[round_robin->requests_size - 1] = to_return;
-	} else {
-		round_robin->requests_size--;
-	}
-	int index = -1;
-	for(int i = 0; i < round_robin->priority_current_size && index == -1; i++) {
-		if(equals(to_return.requester, round_robin->priority_queue[i])) {
-			index = i;
-		}
-	}
-	for(int i = index + 1; i < round_robin->priority_current_size; i++) {
-		round_robin->priority_queue[i - 1] = round_robin->priority_queue[i];
-	}
-	round_robin->priority_queue[round_robin->priority_current_size - 1] = to_return.requester;
+	to_return->request_next = NULL;
 
-	return to_return.requester;
+	//Priority list management
+	if(to_return->priority_next != NULL) {
+		round_robin->priority_first = to_return->priority_next;
+		round_robin->priority_last->priority_next = to_return;
+	}
+	round_robin->priority_last = to_return;
+	to_return->priority_next = NULL;
+
+	return to_return->requester;
 }
 
-bool is_id_valid (requester_t requester){
-	return (requester.id->id != -1);
+static void free_helper(round_robin_adt round_robin, struct request_t* current) {
+	if(current != NULL)
+		free_helper(round_robin, current->priority_next);
+	else 
+		return;
+	free(current->requester->id);
+	free(current);
+}
+
+void free_round_robin(round_robin_adt round_robin) {
+	struct request_t* current = round_robin->priority_first;
+	free_helper(round_robin, current);
+	free(round_robin);
+}
+
+int main(void) {
+	round_robin_adt rr = new_round_robin();
+	requester_t req[3];
+
+	for(int i = 0; i < 3; i++) {
+		req[i].fd = i + 1;
+		instantiate_requester(rr, &req[i]);
+	}
+
+	push(rr, &req[0]);
+	print_list(rr);
+	push(rr, &req[0]);
+	print_list(rr);
+	push(rr, &req[1]);
+	print_list(rr);
+	push(rr, &req[2]);
+	print_list(rr);
+	pop(rr);
+	print_list(rr);
+	pop(rr);
+	print_list(rr);
+	pop(rr);
+	print_list(rr);
+	pop(rr);
+	print_list(rr);
 }
